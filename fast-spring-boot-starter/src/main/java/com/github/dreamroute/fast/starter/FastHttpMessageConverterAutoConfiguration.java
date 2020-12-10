a8a111925073b26f624c698013469af7901347de
w.dehai <342252328@qq.com>
2020-12-10 15:29:36 +0800

    feat: 创建starter

--- fast-spring-boot-starter/src/main/java/com/github/dreamroute/fast/starter/FastHttpMessageConverterAutoConfiguration.java
+++ fast-spring-boot-starter/src/main/java/com/github/dreamroute/fast/starter/FastHttpMessageConverterAutoConfiguration.java
@@ -2,10 +2,12 @@ package com.github.dreamroute.fast.starter;
 
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -34,12 +36,13 @@ import static org.springframework.http.MediaType.TEXT_PLAIN;
 import static org.springframework.http.MediaType.TEXT_XML;
 
 /**
- * JSON转换
+ * FastJSON转换
  *
  * @author w.dehai
  */
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+@ConditionalOnClass(ConfigurableWebApplicationContext.class)
+public class FastHttpMessageConverterAutoConfiguration implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.removeIf(conv -> conv instanceof MappingJackson2HttpMessageConverter);
