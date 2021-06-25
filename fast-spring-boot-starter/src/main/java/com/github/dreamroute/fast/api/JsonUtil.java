50ec333d6d2a26fa69076c0afc5e8c441c3b6d35
w.dehai <w.dehai>
2021-06-25 16:13:24 +0800

    feat: 增加JSON序列化/反序列化工具类

---
+++ fast-spring-boot-starter/src/main/java/com/github/dreamroute/fast/api/JsonUtil.java
@@ -0,0 +1,60 @@
+package com.github.dreamroute.fast.api;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
+import java.util.List;
+
+import static com.alibaba.fastjson.JSON.parseArray;
+import static com.alibaba.fastjson.JSON.parseObject;
+import static com.alibaba.fastjson.JSON.toJSONString;
+
+/**
+ * json工具类，用于对象的序列化和反序列化，工具类内部主要是解决枚举问题的序列化和反序列化问题。
+ * 封装了FastJson的JSON.toJSONString、JSON.parseObject和JSON.parseArray方法
+ *
+ * @author w.dehai.2021/6/25.15:38
+ */
+public class JsonUtil {
+    private JsonUtil() {}
+
+    /**
+     * 序列化对象：将对象转换成json字符串
+     *
+     * @param object pojo对象
+     * @return 返回json字符串
+     */
+    public static String toJsonStr(Object object) {
+        return toJSONString(object, new EnumSerializer());
+    }
+
+    /**
+     * 序列化对象，可以自定义序列化方案
+     *
+     * @param object pojo对象
+     * @param features FastJson的序列化特性参数
+     */
+    public static String toJsonStr(Object object, SerializerFeature... features) {
+        return toJSONString(object, new EnumSerializer(), features);
+    }
+
+    /**
+     * 反序列化对象：将字符串转换成pojo对象
+     *
+     * @param input json字符串
+     * @param clazz pojo类型
+     */
+    public static <T> T parseObj(String input, Class<T> clazz) {
+        return parseObject(input, clazz, new EnumParserConfig());
+    }
+
+    /**
+     * 反序列化列表：将字符串转换成pojo列表
+     *
+     * @param input json字符串
+     * @param clazz pojo类型
+     */
+    public static <T> List<T> parseArr(String input, Class<T> clazz) {
+        return parseArray(input, clazz, new EnumParserConfig());
+    }
+
+}
---
+++ fast-spring-boot-starter/src/test/java/com/github/dreamroute/fast/api/JsonUtilTest.java
@@ -0,0 +1,59 @@
+package com.github.dreamroute.fast.api;
+
+import com.github.dreamroute.mybatis.pro.core.typehandler.EnumMarker;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.github.dreamroute.fast.api.JsonUtil.parseObj;
+import static com.github.dreamroute.fast.api.JsonUtil.toJsonStr;
+import static com.github.dreamroute.fast.api.JsonUtilTest.Gender.MALE;
+
+/**
+ * @author w.dehai.2021/6/25.15:55
+ */
+class JsonUtilTest {
+
+    @Getter
+    @AllArgsConstructor
+    enum Gender implements EnumMarker {
+        MALE(1, "男"),
+        FEMALE(2, "女");
+
+        private final Integer value;
+        private final String desc;
+    }
+
+    @Data
+    static class User {
+        private Long id;
+        private Gender gender;
+    }
+
+    @Test
+    void toJsonStrTest() {
+        User user = new User();
+        user.setId(100L);
+        user.setGender(MALE);
+        String result = toJsonStr(user);
+        String expect = "{\"gender\":1,\"id\":100}";
+        Assertions.assertEquals(expect, result);
+    }
+
+    @Test
+    void parseObjTest() {
+        String input = "{\"gender\":1,\"id\":100}";
+        User user = parseObj(input, User.class);
+        User expect = new User();
+        expect.setId(100L);
+        expect.setGender(MALE);
+        Assertions.assertEquals(expect, user);
+    }
+
+    @Test
+    void parseArrTest() {
+        // ignore.
+    }
+}
