6487db49f11d28332b714f8be9f9d4fa4693828b
w.dehai <w.dehai>
2021-12-06 18:32:28 +0800

    refactor: 调整路径，以及移除RequestAdvice

--- fast-spring-boot-starter/src/main/java/com/github/dreamroute/fast/api/serializer/EnumSerializer.java
+++ fast-spring-boot-starter/src/main/java/com/github/dreamroute/fast/api/serializer/EnumSerializer.java
@@ -2,6 +2,10 @@ package com.github.dreamroute.fast.api.serializer;
 
 import com.alibaba.fastjson.serializer.ValueFilter;
 import com.github.dreamroute.mybatis.pro.base.EnumMarker;
+import org.springframework.cglib.beans.BeanGenerator;
+import org.springframework.cglib.beans.BeanMap;
+
+import java.lang.reflect.Field;
 
 /**
  * 枚举序列化时候，将枚举类型转换成value类型
@@ -12,6 +16,20 @@ public class EnumSerializer implements ValueFilter {
     @Override
     public Object process(Object object, String name, Object value) {
         if (value instanceof EnumMarker) {
+            BeanGenerator generator = new BeanGenerator();
+            generator.setSuperclass(object.getClass());
+            generator.addProperty("desc", String.class);
+            Object target = generator.create();
+            BeanMap beanMap = BeanMap.create(target);
+            beanMap.put("desc", "ms");
+            System.err.println(target);
+            object = target;
+            try {
+                Field[] fields = target.getClass().getFields();
+                System.err.println(fields);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             return ((EnumMarker) value).getValue();
         }
         return value;
