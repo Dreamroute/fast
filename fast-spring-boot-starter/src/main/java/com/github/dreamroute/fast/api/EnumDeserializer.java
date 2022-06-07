22d178349457314ca5d9161ce13fcd16f8af5133
王德海 <wangdehai@yzw.cn>
2022-06-07 16:02:01 +0800

    fix: 处理异常npe

--- fast-spring-boot-starter/src/main/java/com/github/dreamroute/fast/api/EnumDeserializer.java
+++ fast-spring-boot-starter/src/main/java/com/github/dreamroute/fast/api/EnumDeserializer.java
@@ -22,6 +22,7 @@ public class EnumDeserializer implements ObjectDeserializer {
         final JSONLexer lexer = parser.lexer;
         Class<EnumMarker> cls = (Class<EnumMarker>) type;
         if (EnumMarker.class.isAssignableFrom(cls)) {
+            // 这里不能使用intVal()或者integerVal()，因为有默认值0，当前端传入null时，会默认成0，造成业务错误
             String v = lexer.stringVal();
             if (StringUtils.isEmpty(v)) {
                 return null;
